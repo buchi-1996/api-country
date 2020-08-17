c68305138cba03b51e210ad5efb46fcc147c7727
buchi-1996 <ukaigweonyebuchi@gmail.com>
2020-08-17 16:48:56 +0100

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Container, Toolbar, IconButton } from "@material-ui/core";
+import { AppBar, Container, Toolbar, Button } from "@material-ui/core";
 import NightsStayOutlinedIcon from "@material-ui/icons/NightsStayOutlined";
 import "./header.css";
 
@@ -11,7 +11,7 @@ function Header() {
           <h3>Where in the world?</h3>
           <div className="toggle__mode">
             <NightsStayOutlinedIcon className="night__icon" />
-            <IconButton>Dark Mode</IconButton>
+            <Button>Dark Mode</Button>
           </div>
         </Container>
       </Toolbar>
